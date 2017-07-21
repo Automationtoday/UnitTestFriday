c29225dcd2d81a9aea4e06eb1c688ba5d4c53e56
Andrew Moss <Andrewmoss12345@outlook.com>
2017-07-21 15:27:31 +0100

    Add project files.

---
+++ UnitTestFriday/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFriday")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Slater & Gordon")]
+[assembly: AssemblyProduct("UnitTestFriday")]
+[assembly: AssemblyCopyright("Copyright © Slater & Gordon 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88b48f40-6dfa-450e-a13e-f37d60ae7018")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
